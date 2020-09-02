e96043a0cee6967e606f2a6a30b54925bff3fbed
Jannis <Jannis@LAPTOP-QQ7QFO8F>
2020-09-02 11:01:20 +0200

    Added Card Constructor

--- src/model/card/Card.java
+++ src/model/card/Card.java
@@ -18,8 +18,14 @@ public class Card {
 
 	private ArrayList<Effect> effects;
 	
-	public Card() {
-		
+	public Card(int age, String name, CardType type, ArrayList<Resource> producing, ArrayList<Resource> required, ArrayList<Card> dependencies, ArrayList<Effect> effects) {
+		this.age = age;
+		this.name = name;
+		this.type = type;
+		this.producing = producing;
+		this.required = required;
+		this.dependencies = dependencies;
+		this.effects = effects;
 	}
 	
 	public int getAge() {

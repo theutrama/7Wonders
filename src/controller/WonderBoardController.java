44e5df0bc863013e1869e4642eb93f33de42dc4f
felix <obenausfelix@googlemail.com>
2020-09-02 09:26:17 +0200

    add Reflection write WonderboardController

--- src/controller/WonderBoardController.java
+++ src/controller/WonderBoardController.java
@@ -1,17 +1,55 @@
 package controller;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Random;
+import org.reflections.Reflections;
 import model.board.WonderBoard;
 
 public class WonderBoardController {
 
+	private Random rand = new Random();
 	private SevenWondersController sevenWondersController;
 
-	public WonderBoard createWonderBoard(Class<? extends WonderBoard> clazz) {
+	private ArrayList<Class<? extends WonderBoard>> boards = new ArrayList<Class<? extends WonderBoard>>();
+	
+	public WonderBoardController() {
+		loadBoardClasses();
+	}
+	
+	public WonderBoard createWonderBoard(Class<? extends WonderBoard> clazz) throws InstantiationException, IllegalAccessException {
+		if(this.boards.contains(clazz)) {
+			this.boards.remove(clazz);
+			return clazz.newInstance();
+		}
 		return null;
+	}
+	
+	public WonderBoard createWonderBoard() throws InstantiationException, IllegalAccessException {
+		Class<? extends WonderBoard> clazz = this.boards.get( randInt(0,this.boards.size()) );
+		return createWonderBoard(clazz);
+	}
+	
+	public int randInt(int min, int max) {
+	    int randomNum = rand.nextInt((max - min) + 1) + min;
+	    return randomNum;
 	}
 
 	private void loadBoardClasses() {
+		Reflections reflections = new Reflections( "model.board" );
+		List<Class<? extends WonderBoard>> moduleClasses = new ArrayList<>( reflections.getSubTypesOf( WonderBoard.class ) );
 
+		Collections.sort(moduleClasses, new Comparator<Class<? extends WonderBoard>>() {
+		    @Override
+		    public int compare(Class<? extends WonderBoard> o1, Class<? extends WonderBoard> o2) {
+		        return o1.getSimpleName().compareTo(o2.getSimpleName());
+		    }
+		});
+		
+		for ( Class<? extends WonderBoard> clazz : moduleClasses ){
+			this.boards.add(clazz);
+		}
 	}
-
 }

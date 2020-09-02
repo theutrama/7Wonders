27da7e32c5cc4ec39ec6b708b293462f642fb175
maxib <maxib@DESKTOP-2033J3T.fritz.box>
2020-09-02 10:31:13 +0200

    Fixed GameBoard

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,6 @@
 package application;
 	
+import controller.SevenWondersController;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -10,10 +11,12 @@ import view.menu.MainMenuViewController;
 public class Main extends Application {
 	
 	public static Stage primaryStage;
+	public static SevenWondersController sevenWondersController;
 	
 	@Override
 	public void start(Stage primaryStage) {
-		this.primaryStage = primaryStage;
+		Main.primaryStage = primaryStage;
+		sevenWondersController = new SevenWondersController();
 		try {
 			MainMenuViewController mainMenuViewController = new MainMenuViewController();
 			Scene scene = new Scene(mainMenuViewController,1000,800);
--- src/controller/SevenWondersController.java
+++ src/controller/SevenWondersController.java
@@ -41,4 +41,8 @@ public class SevenWondersController {
 		return null;
 	}
 
+	public Game getGame() {
+		return game;
+	}
+
 }

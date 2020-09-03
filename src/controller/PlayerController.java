06199ff88ec67d1859fbadee6111b6a581d8a3ef
felix <obenausfelix@googlemail.com>
2020-09-03 09:53:44 +0200

    add createPlayer/AI

--- src/controller/PlayerController.java
+++ src/controller/PlayerController.java
@@ -1,28 +1,35 @@
 package controller;
 
+import model.board.WonderBoard;
 import model.card.Card;
+import model.player.AI;
+import model.player.Difficulty;
 import model.player.Player;
 
 public class PlayerController {
 
 	private SevenWondersController swController;
+	private WonderBoardController wb;
 
 	public PlayerController(SevenWondersController swController) {
 		this.swController=swController;
+		this.wb = swController.getWonderBoardController();
 	}
 	
-	public Player createPlayer(String playername) {
-//		Player player = new Player(name, board);
-
-		return null;
+	public Player createPlayer(String playername,String wonderboard) {
+		WonderBoard board = wb.createWonderBoard(wonderboard);
+		Player player = new Player(playername, board);
+		return player;
 	}
 
 	public Player getPlayer(String playername) {
 		return null;
 	}
 
-	public Player createAI(String playername) {
-		return null;
+	public AI createAI(String playername,String wonderboard,Difficulty difficulty) {
+		WonderBoard board = wb.createWonderBoard(wonderboard);
+		AI ai = new AI(difficulty, board);
+		return ai;
 	}
 
 	public void chooseCard(Card card, Player player) {

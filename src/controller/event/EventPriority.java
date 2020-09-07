f3476041062f714238f815fd5978c936c0d45786
Jannis <Jannis@LAPTOP-QQ7QFO8F>
2020-09-07 12:38:02 +0200

    Created JavaDoc for EventPriority

--- src/controller/event/EventPriority.java
+++ src/controller/event/EventPriority.java
@@ -1,19 +1,27 @@
 package controller.event;
 
+/** EventPriority which sets the priority of the event */
 public enum EventPriority {
-LOWEST(0), //Wird als erstes ausgef�hrt
-LOW(1),
-MEDIUM(2),
-HIGH(3),
-HIGHEST(4); //Wird als letztes ausgef�hrt
+	LOWEST(0), //Wird als erstes ausgefuehrt
+	LOW(1),
+	MEDIUM(2),
+	HIGH(3),
+	HIGHEST(4); //Wird als letztes ausgefuehrt
 
-private int priority=0;
-EventPriority(int priority){
-	this.priority=priority;
-}
-
-public int getPriority() {
-	return this.priority;
-}
-
+	/** priority of Event */
+	private int priority=0;
+	/**
+	 * creates new EventPriority
+	 * @param priority
+	 */
+	EventPriority(int priority){
+		this.priority=priority;
+	}
+	/**
+ 	* getter for {@link #priority}
+ 	* @return priority
+ 	*/
+	public int getPriority() {
+		return this.priority;
+	}
 }

80ad5102e8f736bd526e5266b979054c00851deb
timow <timow@DESKTOP-O0LS9PO.fritz.box>
2020-09-08 14:55:45 +0200

    add CardControllerTest

---
+++ tests/controller/CardControllerTest.java
@@ -0,0 +1,48 @@
+package controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import application.Main;
+
+public class CardControllerTest {
+	
+	private SevenWondersController swc;
+	private GameController gC;
+	private PlayerController pC;
+	private CardController cC;
+	
+	/**
+	 * Setup test methods
+	 */
+	@Before
+	public void setUp() {
+		
+		swc = SevenWondersFactory.create();
+		gC = swc.getGameController();
+		cC = swc.getCardController();
+		pC = swc.getPlayerController();
+		Main.TEST = true;
+	}
+
+	@Test
+	public void createHasCardTest() {
+		assertEquals(true,cC.hasCard(swc.getPlayerController().getPlayer("erster"), "library"));
+		assertEquals(false,cC.hasCard(swc.getPlayerController().getPlayer("erster"), "academy"));
+	}
+	
+	@Test
+	public void createSellCardTest() {
+		//Wenn bei getCard ein falscher name angegeben wird kommt null zurück 
+		cC.sellCard(cC.getCard(pC.getPlayer("erster").getHand(), "lodge"), pC.getPlayer("erster"));
+		assertEquals(pC.getPlayer("erster").getCoins(),6);
+		cC.sellCard(cC.getCard(pC.getPlayer("erster").getHand(), "study"), pC.getPlayer("erster"));
+		assertEquals(pC.getPlayer("erster").getCoins(),9);
+		
+	}
+	
+	
+
+}

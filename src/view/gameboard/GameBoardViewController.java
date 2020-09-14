189b2b0aa8db53192ade85c68094236464702bd6
Leon Bartmann <leon.bartmann@tu-dortmund.de>
2020-09-14 11:31:30 +0200

    push

--- src/view/gameboard/GameBoardViewController.java
+++ src/view/gameboard/GameBoardViewController.java
@@ -413,7 +413,7 @@ public class GameBoardViewController extends VBox {
 			}
 		});
 		try {
-			Thread.sleep(5000);
+			Thread.sleep(3000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}

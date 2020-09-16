76f7bb6dc7cc3c51aa0b7216560373e30f86e5e5
felix <obenausfelix@googlemail.com>
2020-09-16 12:42:14 +0200

    update

--- src/view/gameboard/GameBoardViewController.java
+++ src/view/gameboard/GameBoardViewController.java
@@ -1109,7 +1109,8 @@ public class GameBoardViewController extends VBox {
 			new Thread(() -> {
 				((ArtInt) player).calculateNextMove();
 				Card selected = ((ArtInt) player).getSelectedCard();
-				Console.log("[" + player.getClass().getSimpleName() + "] action: " + ((ArtInt) player).getAction() + "  card: " + ((ArtInt) player).getSelectedCard() + "  trade: " + ((ArtInt) player).getTradeOption());
+				if(!(player instanceof Multiplayer))
+					Console.log("[" + player.getClass().getSimpleName() + "] action: " + ((ArtInt) player).getAction() + "  card: " + ((ArtInt) player).getSelectedCard() + "  trade: " + ((ArtInt) player).getTradeOption());
 				int index = indexOf(player.getHand(), selected);
 
 				VBox vbox = (VBox) hboxCards.getChildren().get(index);
